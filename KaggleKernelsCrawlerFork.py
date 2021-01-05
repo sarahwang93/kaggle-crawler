faf1f310802bd80e18af861881e5b49c6e1c7736
sarahwang93 <wanggeo93@gmail.com>
2021-01-05 12:15:15 -0500

    add more levels

--- KaggleKernelsCrawlerFork.py
+++ KaggleKernelsCrawlerFork.py
@@ -9,39 +9,13 @@ from time import time, sleep
 import multiprocessing
 from multiprocessing import Pool, Process, Manager
 from datetime import datetime
+from kagglesingle import kagglesingle
 
 global_lock = threading.Lock()
 
-def testsingle(url,count,outputstring):
-    fork_parent_url_combine = ''
-    count += 1
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:39.0)'}
-    res = requests.get(url=url, headers=headers)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    html = str(soup.body)
-    pq = PyQuery(html)
-    tag = pq('script.kaggle-component')
-    testsen = tag[1].text
-    forknum = re.findall(r'forkCount\":(.*?),', testsen)[0]
-    forkParent = re.findall(r'forkParent\":(.*?),', testsen)[0]
-    if forkParent == 'null':
-        outputstring += url + " " + " forknum: " + forknum + " " + "level: " + str(count)
-        return
-    else:
-        # print("test" + forkParent)
-        fork_parent_info = re.findall(r'forkParent\":(.*?)}', testsen)[0]
-        # when fork parent is private the forkParentID is 0
-        if re.findall(r'kernelId\":(.*?),', fork_parent_info)[0] != '0':
-            fork_parent_url = re.findall(r'url\":(.*?),', fork_parent_info)[0]
-            fork_parent_url_combine = "https://www.kaggle.com" + fork_parent_url.replace('\"', '')
-            if fork_parent_url == '':
-                pass
-            else:
-                outputstring += fork_parent_url_combine + " forknum: " + forknum + " level: " + str(count)
-                testsingle(fork_parent_url_combine, count, outputstring)
-    return outputstring
 
 def read_write(inputurls, output, procenum):
+    startPro = kagglesingle()
     while global_lock.locked():
         sleep(0.01)
         continue
@@ -53,9 +27,11 @@ def read_write(inputurls, output, procenum):
             temp = line.split(",")
             url = "https://www.kaggle.com/" + temp[0] + '/' + temp[1].replace('\n', '')
             try:
-                outputstr = ""
-                startcount = 0
-                outputf.write(str(testsingle(url, startcount, outputstr)) + '\n')
+                startCount = 0
+                startString = ""
+                visitedArr = []
+                returnVal = [None] * 2
+                outputf.write(str(startPro.testsingleurl(url, startCount, startString, visitedArr, returnVal)) + '\n')
                 outputf.flush()
             except Exception as e:
                 print(e)
--- kagglesingle.py
+++ kagglesingle.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup
 from pyquery import PyQuery
 import re
 
+
 class kagglesingle:
-    def testsingleurl(self, url, count, outputstr, visited):
+    def testsingleurl(self, url, count, outputstr, visited, return_arr):
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:39.0)'}
         try:
             res = requests.get(url=url, headers=headers)
@@ -21,36 +22,41 @@ class kagglesingle:
         fork_parent_info = re.findall(r'forkParent\":(.*?)}', testsen)[0]
         # when fork parent is private the forkParentID is 0
         count += 1
+
         if url not in visited:
             visited.append(url)
         else:
             pass
+
         if forkParent == 'null':
             outputstr += url + " " + " forknum: " + forknum + " " + "level: " + str(count) + " "
             if len(visited) == count:
                 print(outputstr)
-                return outputstr
+                return_arr[0] = visited[0]
+                return_arr[1] = count
+            return return_arr
         else:
             if re.findall(r'kernelId\":(.*?),', fork_parent_info)[0] != '0':
                 fork_parent_url = re.findall(r'url\":(.*?),', fork_parent_info)[0]
                 fork_parent_url_combine = "https://www.kaggle.com" + fork_parent_url.replace('\"', '')
                 if fork_parent_url == "":
-                    return
+                    pass
                 else:
-                    #TO-DO: how to end recursive call from here
+                    # TO-DO: how to end recursive call from here
                     if fork_parent_url_combine not in visited:
                         outputstr += url + " forknum: " + forknum + " level: " + str(count) + " "
                         if count <= len(visited):
-                            self.testsingleurl(fork_parent_url_combine, count, outputstr, visited)
-                    else:
-                        pass
+                            self.testsingleurl(fork_parent_url_combine, count, outputstr, visited, return_arr)
+
+            return return_arr
+
 
 if __name__ == '__main__':
     test_url = kagglesingle()
     startStr = ""
+    returnArr = [None] * 2
     singleUrl = "https://www.kaggle.com/snsinha0305/higher-lb-score-by-tuning-mloss-around-6-811"
     startCount = 0
     visitedArr = []
-    print("test: " + str(test_url.testsingleurl(singleUrl, startCount, startStr, visitedArr)))
+    print("test: " + str(test_url.testsingleurl(singleUrl, startCount, startStr, visitedArr, returnArr)))
     print(visitedArr)
-
